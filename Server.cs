1941a7c3fa406b348030d808bc35d782b83f6b54
Tadayo <daniel.reffeldt@outlook.com>
2020-06-29 21:39:27 +0200

    made Server.cs

---
+++ Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cyberpunk2020Library;
+
+namespace Cyberpunk2020CC
+{
+    internal static class Server
+    {
+
+        static Character[] GetCharactersFromDatabase()
+        {
+            return null;
+        }
+    }
+}
